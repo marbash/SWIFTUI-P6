ac915ef6a1da2bbf5a21a0c1e39b73ae32c39d8a
Mohammed Alarbash <mohammed@arbash.net>
2020-05-11 22:43:36 +0300

    animated button

--- SWIFTUI P6/ContentView.swift
+++ SWIFTUI P6/ContentView.swift
@@ -9,9 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var width: CGFloat = 60
+    @State var tapped: Bool = false
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Button(action: addItem) {
+                ZStack{
+                    Capsule().foregroundColor(Color.purple)
+                    HStack {
+                        Image(systemName: "plus")
+                            .foregroundColor(.white)
+                            .font(.system(size: 25, weight: .bold, design: .rounded))
+                        if tapped{
+                            Text("Add new item")
+                                .font(.custom("Avenir Next Demi Bold", size: 18))
+                                .foregroundColor(.white)
+                            Spacer()
+                        }
+                        
+                    }.offset(x: tapped ? 20 : 0)
+                    
+                }.frame(width: width, height: 60, alignment: .leading)
+            }.animation(.interactiveSpring(response: 0.4, dampingFraction: 0.4, blendDuration: 0.5))
+            
+        }
+
     }
+    
+    func addItem(){
+        // When tapping button function here
+        tapped.toggle()
+        if tapped{
+            width = 300
+        }else{
+            width = 60
+        }
+        
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
